a64bfee79a3e6b439827c986f1c2f568b0dd5997
Your Name <you@example.com>
2023-12-11 21:43:47 +0200

    add ToDo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import {Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+const LazyHome = React.lazy(() => import('./Component/Home'));
+const LazyToDo = React.lazy(() => import('./Component/ToDo'));
+
+
+
 
 function App() {
   return (
-    <nav>
-      <link></link>
+    <div>
+      <nav>
+        <t><Link to="/Home">Home</Link></t>
+        <t><Link to="/ToDo">ToDo</Link></t>
+      </nav>
+      <Routes>
+        <Route
+          path="/Home"
+          element={
+            <Suspense fallback={<h1>loading...</h1>}><LazyHome /></Suspense>} />
+        <Route path="/ToDo" element={<Suspense fallback={<h1>loading...</h1>}>  <LazyToDo /></Suspense>} />
+      </Routes>
+    </div>
 
-    </nav>
   );
 }
 
 export default App;
-  
--- src/Component/ToDo.jsx
+++ src/Component/ToDo.jsx
@@ -1,22 +1,51 @@
 import React, { useEffect, useState } from "react"
 const ToDo = () => {
 
-    const [task, setTask] = useState([]);
+    const [tasks, setTasks] = useState([
+        { id: 1, text: 'משימה 1', completed: false },
+        { id: 2, text: 'משימה 2', completed: false },
+        // נוסיף משימות נוספות כרצונך
+    ]);
 
-    const useEffect=(() => {
 
-        task
-    }, []);
+    const handleCheckboxChange = (taskId) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === taskId ? { ...task, completed: !task.completed } : task
+            )
+        );
+    };
+    // const useEffect = (() => {
+
+    //     task
+    // }, []);
 
 
     return (
-        <>
+
+        <div>
+            <h1>רשימת משימות</h1>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <input
+                            type="checkbox"
+                            checked={task.completed}
+                            onChange={() => handleCheckboxChange(task.id)}
+                        />
+                        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+                            {task.text}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+
             <button>הוספת משימה</button>
             <button>עריכת משימה</button>
             {/* <button>שמור</button> */}
             <button>מחיקת משימה</button>
-        </>
 
+        </div>
     )
 };
 export default ToDo;
